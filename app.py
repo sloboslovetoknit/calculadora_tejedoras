bea686570a4cc68b2cbdfffaca904a8db27fb5b6
Susana Lobos G <s.lobos.garcia@gmail.com>
2026-09-21 11:19:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ st.markdown("""
 col_logo, col_titulo = st.columns([1, 3])
 with col_logo:
     try:
-        st.image("Logo.png", width=120)
+        st.image("Logo.png.png", width=120)
     except:
         st.markdown("# 🧶")
 
