51d0d752955c3e189afb761187c502bc4dbc5e87
Susana Lobos G <s.lobos.garcia@gmail.com>
2026-09-21 11:44:06 -0300

    Update app.py

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ st.markdown("""
     /* RECUADRO INICIAL DE ADVERTENCIA EN AZUL CLARO DIRECTO */
     .recuadro-azul-advertencia {
         background-color: #e3f2fd !important;
-        color: #0c5460 !important;
+        color: #e3f2fd !important;
         border: 1px solid #b8daff !important;
         border-radius: 8px !important;
         padding: 15px !important;
